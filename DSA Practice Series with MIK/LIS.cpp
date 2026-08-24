0a41ebc5c81b5662e9bc1b2174f9e5332b23bc98
Harsh Kumar <harshkkr1.ug24.cs@nitp.ac.in>
2026-08-24 07:54:51 +0530

    implement patience sorting in O(Nlog(N)) T.C.  and also learn about STL lower_bound in C++ to find the just greater than or equal to element from starting

--- DSA Practice Series with MIK/LIS.cpp
+++ DSA Practice Series with MIK/LIS.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+        int binary_search_to_find_lower_bound(vector<int>&sorting,int target){
+            int i=0;
+            int j=sorting.size()-1;
+            int storeIdx=-1;
+            while(i<=j){
+                int mid=i+(j-i)/2;
+                if(sorting[mid]<target){
+                    i=mid+1;
+                }else{
+                    storeIdx=mid;
+                    j=mid-1;
+                }
+            }
+            return storeIdx;
+        }
     // LIS (very famous DP Question)
     // int dp[2501][2501]; // we store the index of the prev element not the
     // exact
@@ -24,9 +39,9 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         // memset(dp, -1, sizeof(dp));
         // return solve(0, nums, -1); // store the index of the prev element
-        // Bottom-Up code  // T.C. is O(N^2) 
-        // int n = nums.size();
-        // vector<int> t(n, 1);
+        // Bottom-Up code  // T.C. is O(N^2)
+        int n = nums.size();
+        // vector<int> t(n, 1);  // t[i] denotes the LIS ending at the ith index
         // int mx = 1;
         // for (int i = 1; i < n; i++) {
         //     for (int j = 0; j < i; j++) {
@@ -38,8 +53,30 @@ public:
         // }
         // return mx;
 
-        // Now suggested Time complexity is O(NlogN) so Do it 
+        // Now suggested Time complexity is O(NlogN) so Do it
+        // Patience Sorting T.C. is O(Nlog(N)) but we have to use the
+        // lower_bound STL to find the jsut greater than or equal to element or
+        // we can also use the BS to find the lower_bound
+        // This is by using the built-in lower_bound function best way
+        // Now Do the same by using Binary_search to find the lower_bound index 
+        vector<int> sorting;
+        // for (int i = 0; i < n; i++) {
+        //     int pos = lower_bound(sorting.begin(), sorting.end(), nums[i]) - sorting.begin();
+        //     if (pos != sorting.size()) {
+        //         sorting[pos] = nums[i];
+        //     } else {
+        //         sorting.push_back(nums[i]);
+        //     }
+        // }
 
-
+        for(int el : nums){
+            int idx=binary_search_to_find_lower_bound(sorting,el);
+            if(idx==-1){
+                sorting.push_back(el);
+            }else{
+                sorting[idx] = el;
+            }
+        }
+        return sorting.size();
     }
 };
